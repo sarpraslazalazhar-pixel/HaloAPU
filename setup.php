29b839d6ec6b4a6627c7caf8690c5869f34d0dc0
Sarpras <sarpras@lazalazhar.org>
2026-07-16 11:46:08 +0800

    benerin setup

--- setup.php
+++ setup.php
@@ -1,9 +1,27 @@
 <?php
 // hapus file ini setelah deploy selesai!
+
+// Cari bootstrap/app.php (baik setup.php di root proyek maupun di folder public/)
+$paths = [
+    __DIR__.'/bootstrap/app.php',
+    __DIR__.'/../bootstrap/app.php',
+];
+
+$app = null;
+foreach ($paths as $path) {
+    if (file_exists($path)) {
+        $app = require $path;
+        break;
+    }
+}
+
+if (!$app) {
+    die('bootstrap/app.php tidak ditemukan. Pastikan setup.php berada di folder proyek.');
+}
+
 $_SERVER['argv'] = ['artisan'];
 $_SERVER['argc'] = 1;
 
-$app = require __DIR__.'/bootstrap/app.php';
 $kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
 $kernel->bootstrap();
 
